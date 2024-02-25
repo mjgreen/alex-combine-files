201def9c85c8f51fea6328f0d5d4faec6c001f6a
mgreen <mgreen@bournemouth.ac.uk>
2024-02-25 15:32:18 +0000

    fix all_data not having all pps

--- alex.R
+++ alex.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-all_filenames <- list.files(path = "individual_csvs/", pattern="*.csv", full.names = TRUE)
+all_filenames <- list.files(path = "individual_csvs", pattern="*.csv", full.names = TRUE)
 all_data <- tibble()
 for (this_filename in all_filenames){
   
@@ -7,8 +7,8 @@ for (this_filename in all_filenames){
   participant_data <- read_csv(this_filename, skip=3, lazy=FALSE, show_col_types = FALSE)
   participant_info <- read_csv(this_filename, n_max=1, show_col_types = FALSE) %>% select(-subjectGroup)
   
-  all_data <- bind_cols(participant_data, participant_info)
-  
+  participant_info_and_data <- bind_cols(participant_data, participant_info)
+  all_data <- all_data %>% bind_rows(participant_info_and_data)
 }
 
 write_csv(all_data, "combined_csvs/all_data.csv")
